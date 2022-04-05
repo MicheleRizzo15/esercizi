9cc1ed9c029f1115f2df2893712bfed48f21c075
MicheleRizzo15 <98811599+MicheleRizzo15@users.noreply.github.com>
2022-04-05 11:12:56 +0200

    arrayCorretto

--- arrayCorrezione.c
+++ arrayCorrezione.c
@@ -4,7 +4,7 @@ int i;
 int k[10];
 printf("Inserisci 10 numeri: \n");
     for(i=0;i<10;i++){
-    printf("numero %d",&i+1);
+    printf("numero %d \n",i+1);
     scanf("%d",&k[i]);
     }
 printf("Ecco i numeri: ");
